2c2ca4f19d63215d76db6640dd8fa1134a5312a7
Everson <56919528+eversonscherrer@users.noreply.github.com>
2022-04-10 06:10:01 +0200

    Update start-topology.sh

--- start-topology.sh
+++ start-topology.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-RTR=/Users/everson/Dropbox/rtr/rtr.jar
-HWSW=/Users/everson/Dropbox/rtr/everson/wtestbeds/full_mesh_ospf/
+# Copyright [2019-2022] Universidade Federal do Espirito Santo
+#                       Instituto Federal do Espirito Santo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#Here you must add the path according your system operation 
+RTR=/<path freertr>/rtr.jar
+#Here you must add the path according your system operation 
+HWSW=/<path hardware and software files>/
 
 tmux new-session -d -s rare 'java -jar '$RTR' routersc '$HWSW'r1-hw.txt '$HWSW'r1-sw.txt'
 tmux split-window -v -t 0 -p 50
